2a1d44634350655940d344851cab760ac7f31a63
Prasad Rathod <48131340+prasad-rathod-R@users.noreply.github.com>
2025-04-07 00:59:30 -0700

    Update App.js nav bar chrck

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import LoginComponent from "./component/LoginComponent";
 import RegisterComponent from "./component/RegisterComponent";
 import AdmissionForm from "./component/AdmissionForm";
 import ContactFetch from "./component/ContactFetch";
-
+import Navbar from "./component/NavBar";
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<RegisterComponent />} />
         <Route path="/login" element={<LoginComponent />} />
+                    <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <Route path="/" element={<DashboardComponent />}>
           <Route path="/admit" element={<AdmissionForm />} />
           <Route path="/contact" element={<ContactFetch />} />
